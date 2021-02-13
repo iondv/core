3cd11c5495da7f84d7ab07fdf4df577f46976eea
Dan Krasilnikov <krasilneg@yandex.ru>
2021-02-13 12:43:16 +1000

    error setup

--- utils/index.js
+++ utils/index.js
@@ -3,5 +3,6 @@ module.exports.empty = require('./empty');
 module.exports.extend = require('./extend');
 module.exports.config = require('./read');
 module.exports.request = require('./request');
+module.exports.errorSetup = require('./error-setup');
 module.exports.strings = require('./strings');
 module.exports.system = require('./system');
